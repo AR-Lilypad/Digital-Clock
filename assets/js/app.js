a449e3fdfebc7213022c36e2b80ccd974e402c42
Angie Roach <angieroach4@gmail.com>
2020-03-24 12:53:14 -0400

    initial files

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// Homework: write a small program that prints the actual time in real time on the screen.
+//       the project should have:
+//       printing the clock in am/pm;
+//       the background-color of the screen changes dynamically;
+//       the whole project is styled
+
+
