e722e292bfd47e00daa8bea8b36f6a3ab78b055f
Angie Roach <angieroach4@gmail.com>
2020-03-24 20:12:27 -0400

    added style & js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,52 @@
-// Homework: write a small program that prints the actual time in real time on the screen.
+// Homework: write a small program that prints the actual date in real time on the screen.
 //       the project should have:
 //       printing the clock in am/pm;
 //       the background-color of the screen changes dynamically;
 //       the whole project is styled
+
+
+// const date = new Date();
+// console.log(date.getHours() % 12);
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+
+
+let clock = document.getElementById("digitalClock");
+let color = document.getElementById("hexColor");
+
+function digiClock() {
+    const date = new Date();
+    let hours = (date.getHours() % 12).toString();
+    let minutes = date.getMinutes().toString();
+    let seconds = date.getSeconds().toString();
+    let amPM = hours < 12 ? "PM" : "AM";
+    hours = hours ? hours : 12;
+
+    if (hours.length < 2) {
+        hours = "0" + hours;
+    }
+
+    if (minutes.length < 2) {
+        minutes = '0' + minutes;
+    }
+
+    if (seconds.length < 2) {
+        seconds = "0" + seconds;
+    }
+
+    let clockString = hours + " : " + minutes + " : " + seconds + " " + amPM;
+    let colorString = "#" + hours + minutes + seconds;
+
+    clock.textContent = clockString;
+    color.textContent = colorString;
+
+    document.body.style.backgroundColor = colorString;
+}
+
+digiClock();
+setInterval(digiClock, 1000);
+
+hours.style.backgroundColor = "#051980"
+hours.style.border = "2px solid white";
 
 
