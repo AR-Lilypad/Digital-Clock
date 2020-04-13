ee6677b443072c88b901e75b26a4983d6beda289
Angie Roach <angieroach4@gmail.com>
2020-04-12 20:15:20 -0400

    corrected the amPM display

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,26 +16,17 @@ let color = document.getElementById("hexColor");
 
 function digiClock() {
     const date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let amPM = hours < 12 ? "AM" : "PM";
+    let hours = date.getHours().toString();
+    let minutes = date.getMinutes().toString();
+    let seconds = date.getSeconds().toString();
+    let amPM = hours >= 12 ? "PM" : "AM";
 
-    if (hours > 12) {
-        hours = hours - 12 && amPM == PM;
-    }
+    hours = hours % 12;
+    hours = hours ? hours : 12;
 
-    if (hours.length < 2) {
-        hours = "0" + hours;
-    }
-
-    if (minutes.length < 2) {
-        minutes = '0' + minutes;
-    }
-
-    if (seconds.length < 2) {
-        seconds = "0" + seconds;
-    }
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
 
     let clockString = hours + " : " + minutes + " : " + seconds + " " + amPM;
     let colorString = "#" + hours + minutes + seconds;
